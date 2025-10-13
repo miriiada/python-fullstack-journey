4c9989d79c200b4d42bdbff61f82e5a0a5ed71ee
miriiada <grawirus@gmail.com>
2025-10-13 20:42:11 +0200

    First commit

---
+++ Day 1/day 1.py
@@ -0,0 +1,29 @@
+# 1 task
+# user_name = input("Enter you name:")
+# user_age = input("Enter you age:")
+#
+# print(f"Hello, {user_name}! You are {user_age} years old.")
+
+# 2 task
+# first_number = int(input("Enter first number:"))
+# second_number = int(input("Enter second number:"))
+#
+# sum = first_number + second_number
+# difference = first_number - second_number
+# multiplication = first_number * second_number
+# division = first_number / second_number
+# remainder = first_number % second_number
+#
+# print(f"Sum: {sum}\n Difference: {difference}\n Multiplication: {multiplication}\n Division: {division}\n Remainder: {remainder}")
+# Можно еще записать через def calculate_operations():
+
+# 3 task
+# user_text = input("Enter a text:")
+# text_length = len(user_text)
+# text_upper = user_text.upper()
+# text_lower = user_text.lower()
+# text_first_char = user_text[0]
+# text_second_char = user_text[-1]
+# text_reversed = user_text[::-1]
+#
+# print(f"String length: {text_length}\n Uppercase: {text_upper}\n Lowercase: {text_lower}\n First character: {text_first_char}\n Last character: {text_second_char}\n Reversed string: {text_reversed}")
